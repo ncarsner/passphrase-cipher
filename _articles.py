1e55b9c0edf67705477c372e6cb96750ca2a86e7
Nicholas Carsner <nicholascarsner@gmail.com>
2023-12-21 16:54:28 -0600

    Added word lists.

---
+++ _articles.py
@@ -0,0 +1,4 @@
+article = [
+     "a", "an", "another", "any", "each", "every", "her", "his", "its", "more", "my", "no", "our", "some", "that", "the", "their", "these",
+     "this", "those", "your"
+     ]
---
+++ _prepositions.py
@@ -0,0 +1,7 @@
+preposition = [
+         "aboard", "about", "above", "across", "after", "against", "along", "amid", "among", "around", "as", "at", "before", "behind", "below",
+         "beneath", "beside", "between", "beyond", "but", "by", "concerning", "considering", "despite", "down", "during", "due to", "except", "for",
+         "from", "in", "inside", "into", "like", "near", "of", "off", "on", "onto", "out", "outside", "over", "past", "regarding", "round", "since",
+         "through", "throughout", "to", "toward", "towards", "under", "underneath", "until", "up", "upon", "with", "within", "without",
+         "according to"
+         ]
